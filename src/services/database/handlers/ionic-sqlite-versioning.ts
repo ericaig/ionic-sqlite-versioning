989693cf659bca2791391dfe0d6852af8d304a14
ericaig <deleaighewi@hotmail.com>
2020-07-20 22:07:12 +0200

    added ionic sqlite versioning db service for CRUD operation on plugin's db table

---
+++ src/services/database/handlers/ionic-sqlite-versioning.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core'
+import DbColumnTypeInterface from 'src/services/interfaces/db-column-type'
+import { DatabaseHelpersService } from '../helpers'
+import IonicSqliteDbVersioningInterface from 'src/services/interfaces/ionic-sqlite-versioning'
+
+const __table__ = 'ionicSqliteVersioning'
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class IonicSqliteDbVersioningService {
+    // private __cache: object
+
+    constructor(
+        private dbHelpers: DatabaseHelpersService
+    ) { }
+
+    // private set cache(value) { this.__cache = value }
+    // private get cache() { return this.__cache }
+
+    private get timestamp() {
+        return Math.floor(Date.now() / 1000)
+    }
+
+    // CRUD
+
+    public async create(props: IonicSqliteDbVersioningInterface) {
+        const { name, version } = props
+
+        const createdAt = this.timestamp
+
+        return this.delete().then(() =>
+            this.dbHelpers.insert(__table__, { name, version, settings: '', createdAt })
+        )
+            .then(() => { })
+    }
+
+    public async addColumn(table: string, columnType: DbColumnTypeInterface) {
+        return this.dbHelpers.addColumn(table, columnType)
+        .then(() => { })
+    }
+
+    public async read() {
+        return this.dbHelpers.get(__table__, {})
+            .then((result: IonicSqliteDbVersioningInterface) => result)
+    }
+
+    public async update(id: number, props: { version: number }) {
+        const updatedAt = this.timestamp
+
+        return this.dbHelpers.update(__table__, { id }, { ...props, updatedAt })
+            .then(() => { })
+    }
+
+    public async delete() {
+        return this.dbHelpers.delete(__table__, {})
+            .then(() => { })
+    }
+}
